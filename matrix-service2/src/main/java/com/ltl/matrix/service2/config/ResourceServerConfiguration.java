cd3c9e57469efba5dfc0cbfe6d46fb7b20fefad9
litinglong <328172957@qq.com>
2020-12-14 23:30:48 +0800

    提交

--- matrix-service2/src/main/java/com/ltl/matrix/service2/config/ResourceServerConfiguration.java
+++ matrix-service2/src/main/java/com/ltl/matrix/service2/config/ResourceServerConfiguration.java
@@ -1,10 +1,13 @@
 package com.ltl.matrix.service2.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @EnableResourceServer
@@ -15,7 +18,11 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/order/**").authenticated(); // 配置order访问控制，必须认证后才可以访问
-        http.authorizeRequests()
-        .antMatchers("/echos/**").permitAll();
+    }
+	
+    @Bean(name="myRestTemplate")
+    @LoadBalanced
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
     }
 }
