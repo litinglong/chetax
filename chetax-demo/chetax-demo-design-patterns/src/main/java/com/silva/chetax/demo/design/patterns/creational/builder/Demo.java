32acc99cd26191ae7d0de4dd0d20ff33ec4b7de9
litinglong <328172957@qq.com>
2021-02-03 16:23:50 +0800

    建造者模式文档

--- chetax-demo/chetax-demo-design-patterns/src/main/java/com/silva/chetax/demo/design/patterns/creational/builder/Demo.java
+++ chetax-demo/chetax-demo-design-patterns/src/main/java/com/silva/chetax/demo/design/patterns/creational/builder/Demo.java
@@ -1,7 +1,9 @@
 package com.silva.chetax.demo.design.patterns.creational.builder;
 
 /**
- * 建造者（Builder）模式的主要角色如下。
+ * http://c.biancheng.net/view/1354.html
+ * 
+ * <p>建造者（Builder）模式的主要角色如下。
  * <p> 产品角色（Product）：它是包含多个组成部件的复杂对象，由具体建造者来创建其各个零部件。
  * <p> 抽象建造者（Builder）：它是一个包含创建产品各个子部件的抽象方法的接口，通常还包含一个返回复杂产品的方法 getResult()。
  * <p> 具体建造者(Concrete Builder）：实现 Builder 接口，完成复杂产品的各个部件的具体创建方法。
