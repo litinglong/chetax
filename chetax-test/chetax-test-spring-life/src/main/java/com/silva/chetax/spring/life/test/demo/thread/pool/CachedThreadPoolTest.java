de959e1c8d4ccec9619abf4c5fd5a17130bb2acd
litinglong <328172957@qq.com>
2021-02-16 22:20:02 +0800

    线程池测试

---
+++ chetax-test/chetax-test-spring-life/src/main/java/com/silva/chetax/spring/life/test/demo/thread/pool/CachedThreadPoolTest.java
@@ -0,0 +1,35 @@
+package com.silva.chetax.spring.life.test.demo.thread.pool;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import com.silva.chetax.spring.life.test.demo.thread.MyCallable;
+import com.silva.chetax.spring.life.test.demo.thread.MyRunnable;
+import com.silva.chetax.spring.life.test.demo.thread.MyThread;
+
+public class CachedThreadPoolTest {
+	public static void main(String[] args) {
+        ExecutorService pool = Executors.newCachedThreadPool();
+        Callable<Integer> callable = new MyCallable();
+        Runnable runnable = new MyRunnable();
+        Thread thread = new MyThread();
+        
+        // 执行任务并获取Future对象
+        Future<Integer> f = pool.submit(callable);
+        pool.submit(runnable);
+        pool.submit(thread);
+        // 关闭线程池
+        pool.shutdown();
+        // 获取所有并发任务的运行结果
+        // 从Future对象上获取任务的返回值，并输出到控制台
+        try {
+			System.out.println("call返回值>>>" + f.get().toString());
+		} catch (InterruptedException | ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} //OPTION + return 抛异常
+	}
+}
---
+++ chetax-test/chetax-test-spring-life/src/main/java/com/silva/chetax/spring/life/test/demo/thread/pool/ThreadPoolExecutorTest.java
@@ -0,0 +1,39 @@
+package com.silva.chetax.spring.life.test.demo.thread.pool;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import com.silva.chetax.spring.life.test.demo.thread.MyCallable;
+import com.silva.chetax.spring.life.test.demo.thread.MyRunnable;
+import com.silva.chetax.spring.life.test.demo.thread.MyThread;
+
+public class ThreadPoolExecutorTest {
+	public static void main(String[] args) {
+        ExecutorService pool = new ThreadPoolExecutor(0, Integer.MAX_VALUE,
+                60L, TimeUnit.SECONDS,
+                new SynchronousQueue<Runnable>());
+        Callable<Integer> callable = new MyCallable();
+        Runnable runnable = new MyRunnable();
+        Thread thread = new MyThread();
+        
+        // 执行任务并获取Future对象
+        Future<Integer> f = pool.submit(callable);
+        pool.submit(runnable);
+        pool.submit(thread);
+        // 关闭线程池
+        pool.shutdown();
+        // 获取所有并发任务的运行结果
+        // 从Future对象上获取任务的返回值，并输出到控制台
+        try {
+			System.out.println("call返回值>>>" + f.get().toString());
+		} catch (InterruptedException | ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} //OPTION + return 抛异常
+	}
+}
