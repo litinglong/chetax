012fe2959f8124d23f77947692f2582ef1e348a1
litinglong <328172957@qq.com>
2021-03-31 13:38:34 +0800

    前端功能添加

--- chetax-vue/config/proxyConfig.js
+++ chetax-vue/config/proxyConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
   proxy: {
         '/apis': {    //将www.exaple.com印射为/apis
-            target: 'https://www.exaple.com',  // 接口域名
+            target: 'http://127.0.0.1:57976',  // 接口域名
             secure: false,  // 如果是https接口，需要配置这个参数
             changeOrigin: true,  //是否跨域
             pathRewrite: {
