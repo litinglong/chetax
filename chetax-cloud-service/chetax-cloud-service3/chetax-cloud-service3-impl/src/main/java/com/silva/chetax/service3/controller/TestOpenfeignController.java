588b845c2d0c1f72c2f30ef89bd18c21022cac08
litinglong <328172957@qq.com>
2021-04-09 22:29:37 +0800

    修改微服务配置

--- chetax-cloud-service/chetax-cloud-service3/chetax-cloud-service3-impl/src/main/java/com/silva/chetax/service3/controller/TestOpenfeignController.java
+++ chetax-cloud-service/chetax-cloud-service3/chetax-cloud-service3-impl/src/main/java/com/silva/chetax/service3/controller/TestOpenfeignController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.alibaba.csp.sentinel.annotation.SentinelResource;
-import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.silva.chetax.service3.serviceclient.Service1client;
 
 @RestController
