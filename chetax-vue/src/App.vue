a7a2e94b71dad730df7b8d156d7351e2b65cad0a
litinglong <328172957@qq.com>
2021-07-11 20:28:20 +0800

    tab页签

--- chetax-vue/src/App.vue
+++ chetax-vue/src/App.vue
@@ -58,6 +58,15 @@
             <el-breadcrumb-item>活动列表</el-breadcrumb-item>
             <el-breadcrumb-item>活动详情</el-breadcrumb-item>
           </el-breadcrumb>
+          <el-tabs v-model="editableTabsValue2" type="card" closable @tab-remove="removeTab">
+            <el-tab-pane
+              v-for="(item) in editableTabs2"
+              :key="item.name"
+              :label="item.title"
+              :name="item.name"
+            >
+            </el-tab-pane>
+          </el-tabs>
         </el-header>
         <el-main><router-view/></el-main>
       </el-container>
@@ -68,7 +77,49 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      editableTabsValue2: '2',
+      editableTabs2: [{
+        title: 'Tab 1',
+        name: '1',
+        content: 'Tab 1 content'
+      }, {
+        title: 'Tab 2',
+        name: '2',
+        content: 'Tab 2 content'
+      }],
+      tabIndex: 2
+    }
+  },
+  methods: {
+    addTab (targetName) {
+      let newTabName = ++this.tabIndex + ''
+      this.editableTabs2.push({
+        title: 'New Tab',
+        name: newTabName,
+        content: 'New Tab content'
+      })
+      this.editableTabsValue2 = newTabName
+    },
+    removeTab (targetName) {
+      let tabs = this.editableTabs2
+      let activeName = this.editableTabsValue2
+      if (activeName === targetName) {
+        tabs.forEach((tab, index) => {
+          if (tab.name === targetName) {
+            let nextTab = tabs[index + 1] || tabs[index - 1]
+            if (nextTab) {
+              activeName = nextTab.name
+            }
+          }
+        })
+      }
+      this.editableTabsValue2 = activeName
+      this.editableTabs2 = tabs.filter(tab => tab.name !== targetName)
+    }
+  }
 }
 </script>
 
